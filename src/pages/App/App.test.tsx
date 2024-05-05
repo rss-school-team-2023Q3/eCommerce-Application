478cb78c35d35d3260d5959816799ec28053953b
Anton <antonogonovskiy@gmail.com>
2024-05-05 21:18:20 +0300

    feat(feature sliced design): add FSD architecture to the project

--- src/pages/App/App.test.tsx
+++ src/pages/App/App.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import App from './App';
-import { render, screen, userEvent } from './utils/test-utils';
+import { render, screen, userEvent } from './../../shared/utils/test-utils';
 
 describe('Simple working test', () => {
   it('the title is visible', () => {
