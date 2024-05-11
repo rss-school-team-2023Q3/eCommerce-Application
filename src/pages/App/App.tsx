81fd7b0f19f00ded4cd813817166b453adf4b256
AntonOgonovskiy <90901486+AntonOgonovskiy@users.noreply.github.com>
2024-05-11 07:15:30 +0300

    AntonOgonovskiy
    * feat(signin): implement signIn page with validation of email & password fields. Add tests
    
    * refactor(signin): change function name

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,28 +1,8 @@
-import { useState } from 'react';
 import './App.css';
+import SignIn from 'pages/SignInPage/SignIn';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((cnt) => cnt + 1)}>
-          count is
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos</p>
-    </>
-  );
+  return <SignIn />;
 }
 
 export default App;
