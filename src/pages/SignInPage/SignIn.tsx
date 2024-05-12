43ff7e2986dd67c2709680bb99a512ca08ef8f06
AntonOgonovskiy <90901486+AntonOgonovskiy@users.noreply.github.com>
2024-05-12 09:55:25 +0300

    RSS-ECOMM-2_01: Implement client-side validation for login form
    * feat(signin): implement signIn page with validation of email & password fields. Add tests
    
    * refactor(signin): change function name
    
    * fix(signin): fix link
    
    * feat(signin): add button to toogle show or hide password

--- src/pages/SignInPage/SignIn.tsx
+++ src/pages/SignInPage/SignIn.tsx
@@ -1,4 +1,8 @@
-import { Button, TextField } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputAdornment from '@mui/material/InputAdornment';
+import { Button, IconButton, TextField } from '@mui/material';
+
 import { useEffect, useState } from 'react';
 import './SignIn.modules.css';
 
@@ -10,34 +14,31 @@ function SignIn() {
     email,
     password,
   });
+  const [isShowPassword, setShowPassword] = useState(false);
   const emailRegexp = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
   const passwordRegexp =
     /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])(?!.*\s).{8,}$/;
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
   const validate = (regexp: RegExp, inputValue: string) => {
     if (regexp.test(inputValue)) {
       setValid(true);
-
       return true;
     }
-
     setValid(false);
-
     return false;
   };
 
   const submitLogInData = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
-
     if (isValid) {
       setUser({
         email,
         password,
       });
       const data = user;
-
       return data;
     }
-
     return true;
   };
 
@@ -63,11 +64,24 @@ function SignIn() {
           autoComplete="off"
           style={{ marginBottom: '10px' }}
           value={password}
-          type="password"
+          type={isShowPassword ? 'text' : 'password'}
           onChange={(e) => setPassword(e.target.value)}
           required
           id="password"
           label="Password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {isShowPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
         <Button
           variant="contained"
@@ -79,7 +93,7 @@ function SignIn() {
       </form>
       <div>
         <a className="login-link" href="/sign_up">
-          Don&apos;t have an account? Sign In
+          Don&apos;t have an account? Sign Up
         </a>
       </div>
     </div>
