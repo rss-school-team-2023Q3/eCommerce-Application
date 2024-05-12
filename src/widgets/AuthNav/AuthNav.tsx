87c4ef727aa044647b93b3a0df6339c30f221259
Oleksii Bolotin <samkiral73@gmail.com>
2024-05-12 16:57:22 +0200

    feat(header.tsx): add header and navigation

---
+++ src/widgets/AuthNav/AuthNav.tsx
@@ -0,0 +1,30 @@
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import LoginIcon from '@mui/icons-material/Login';
+import { Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+export default function AuthNav() {
+  return (
+    <div>
+      <NavLink to="/signup">
+        <Button
+          startIcon={<HowToRegIcon />}
+          sx={{ color: 'white' }}
+          variant="outlined"
+        >
+          Register
+        </Button>
+      </NavLink>
+
+      <NavLink to="/signin">
+        <Button
+          startIcon={<LoginIcon />}
+          sx={{ color: 'white' }}
+          variant="outlined"
+        >
+          LogIn
+        </Button>
+      </NavLink>
+    </div>
+  );
+}
