6eb7d243640061204cebe6732227d31c23688c8d
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-05-19 00:25:07 +0300

    test: add several tests

---
+++ src/pages/App/tests/commercetools/tokenCache.test.ts
@@ -0,0 +1,46 @@
+import { TokenStore } from '@commercetools/sdk-client-v2';
+import { tokenCache } from 'shared/libs/commercetools/tokenCache';
+import {
+  describe, expect, it, beforeEach,
+} from 'vitest';
+
+describe('MyTokenCache', () => {
+  beforeEach(() => {
+    tokenCache.clear();
+  });
+
+  it('should start with an empty cache', () => {
+    const cache = tokenCache.get();
+
+    expect(cache.token).toEqual('');
+    expect(cache.expirationTime).toEqual(0);
+    expect(cache.refreshToken).toEqual('');
+  });
+
+  it('should set and get the cache correctly', () => {
+    const newCache: TokenStore = {
+      token: 'token',
+      expirationTime: 12345,
+      refreshToken: 'refreshToken',
+    };
+
+    tokenCache.set(newCache);
+    expect(tokenCache.get()).toEqual(newCache);
+  });
+
+  it('should clear the cache correctly', () => {
+    const newCache: TokenStore = {
+      token: 'token',
+      expirationTime: 12345,
+      refreshToken: 'refreshToken',
+    };
+
+    tokenCache.set(newCache);
+    tokenCache.clear();
+    const cache = tokenCache.get();
+
+    expect(cache.token).toEqual('');
+    expect(cache.expirationTime).toEqual(0);
+    expect(cache.refreshToken).toEqual('');
+  });
+});
--- src/pages/App/tests/utilsTest/selectCountryCode.test.ts
+++ src/pages/App/tests/utilsTest/selectCountryCode.test.ts
@@ -2,7 +2,7 @@ import selectCountryCode from 'shared/utils/selectCountryCode';
 import { describe, expect, it } from 'vitest';
 
 describe('selectCountryCode', () => {
-  it('возвращает правильный код страны для известных стран', () => {
+  it('should return correct country code', () => {
     expect(selectCountryCode('Great Britain')).toBe('GB');
     expect(selectCountryCode('Germany')).toBe('DE');
     expect(selectCountryCode('Canada')).toBe('CA');
