f45eb99019b4aee9cf488b8a06ff69894a5d1680
Oleksii Bolotin <samkiral73@gmail.com>
2024-05-20 13:53:39 +0200

    feat(notfoundpage): add background for page

--- src/pages/NotFoundPage/NotFound.tsx
+++ src/pages/NotFoundPage/NotFound.tsx
@@ -1,15 +1,22 @@
-import { Button } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+
+import Image from './assets/NotFound.svg';
 
 export default function NotFound() {
   return (
-    <>
-      <h2>Not found</h2>
+    <Container
+      style={{ background: `url(${Image}) center no-repeat`, height: '92vh' }}
+    >
       <NavLink to="/main">
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3, py: 2, px: 4 }}
+        >
           Return Home
         </Button>
       </NavLink>
-    </>
+    </Container>
   );
 }
