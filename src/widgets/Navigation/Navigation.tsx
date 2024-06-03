423fff1c718c9e7bb35f23f79e603386713547fb
AntonOgonovskiy <90901486+AntonOgonovskiy@users.noreply.github.com>
2024-06-04 00:14:59 +0300

    RSS-ECOMM-3_21: Implement Navigation  to Catalog Page in Header (#51)
    * fix(fix): fix
    
    * feat(header): add link to catalog in header
    
    * fix(change country name): fix country name

--- src/widgets/Navigation/Navigation.tsx
+++ src/widgets/Navigation/Navigation.tsx
@@ -1,31 +1,50 @@
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import HomeIcon from '@mui/icons-material/Home';
-import { Toolbar, Button } from '@mui/material';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import { Toolbar, Button, Tooltip } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import './Navigation.modules.css';
 
 export default function Navigation() {
   return (
     <Toolbar>
       <NavLink to="/main">
-        <Button
-          startIcon={<HomeIcon />}
-          sx={{ color: 'white' }}
-          variant="outlined"
-        >
-          Home
-        </Button>
+        <Tooltip title="Home">
+          <Button
+            startIcon={<HomeIcon />}
+            className="nav-button"
+            variant="outlined"
+            sx={{ color: 'white' }}
+          >
+            <span className="button-text">Home</span>
+          </Button>
+        </Tooltip>
       </NavLink>
 
-      {/* {isLoggedIn && (
-        <NavLink to="/products">
-          <MUI.Button
-            startIcon={<RecentActorsIcon />}
+      <NavLink to="/catalog">
+        <Tooltip title="Catalog">
+          <Button
+            startIcon={<FormatListBulletedIcon />}
+            className="nav-button"
             sx={{ color: 'white' }}
             variant="outlined"
           >
-            Products
-          </MUI.Button>
-        </NavLink>
-      )} */}
+            <span className="button-text">Catalog</span>
+          </Button>
+        </Tooltip>
+      </NavLink>
+      <NavLink to="/profile">
+        <Tooltip title="Profile">
+          <Button
+            sx={{ color: 'white' }}
+            className="nav-button"
+            startIcon={<ManageAccountsIcon />}
+            variant="outlined"
+          >
+            <span className="button-text">Profile</span>
+          </Button>
+        </Tooltip>
+      </NavLink>
     </Toolbar>
   );
 }
