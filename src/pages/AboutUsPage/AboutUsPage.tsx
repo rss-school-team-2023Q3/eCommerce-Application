00af74c6e566152458b44ced784a52cde9da2f20
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-06-15 18:20:03 +0300

    feat: implement about us page

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,6 +1,95 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import {
+  Toolbar, Typography, Link, Button, Grid,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import TeamMemberCard from 'shared/components/TeamMemberComponent/TeamMemberComponent';
+
+import { teamMembers } from './data.ts';
+
 function AboutUsPage() {
+  const navigate = useNavigate();
+
   return (
-    <h2>About us page</h2>
+    <>
+      <Toolbar
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '100%',
+          marginBottom: '20px',
+          paddingX: 0,
+        }}
+      >
+        <Button
+          startIcon={<ArrowBackIcon />}
+          onClick={() => navigate(-1)}
+          variant="contained"
+        >
+          Back
+        </Button>
+        <Typography
+          fontFamily="Montserrat"
+          variant="h4"
+          fontWeight={600}
+          sx={{ flexGrow: 1, color: '#1569bd' }}
+        >
+          About us
+        </Typography>
+        <Link href="https://rs.school/">
+          <img src="./rs-school-logo.svg" alt="rs-school logo" width={40} />
+        </Link>
+      </Toolbar>
+      <Grid container flexDirection="column" alignItems="center">
+        <Typography
+          fontFamily="Montserrat, sans-serif"
+          fontWeight={500}
+          variant="h5"
+          marginBottom={2}
+          marginLeft={5}
+          justifyItems="flex-end"
+        >
+          Collaboration & Success
+        </Typography>
+        <Typography
+          fontFamily="Montserrat, sans-serif"
+          variant="subtitle1"
+          color="#666"
+          marginBottom={5}
+        >
+          Our team worked well together through clear talks, meetings, and
+          checking each other’s work, which helped us keep learning and getting
+          better. We used agile methods to be able to quickly adjust to any new
+          changes. Our GitHub repository was key for combining code, tracking
+          issues, and managing versions, keeping everyone updated. Our different
+          skills came together to finish our project successfully.
+        </Typography>
+        <Typography
+          fontFamily="Montserrat, sans-serif"
+          fontWeight={500}
+          variant="h5"
+          marginBottom={2}
+          marginLeft={5}
+        >
+          Our Team
+        </Typography>
+      </Grid>
+      <Grid container spacing={5} sx={{ justifyContent: 'center' }}>
+        {teamMembers.map((member) => (
+          <Grid item key={member.name} sx={{}}>
+            <TeamMemberCard
+              name={member.name}
+              photo={member.photo}
+              role={member.role}
+              bio={member.bio}
+              contributions={member.contributions}
+              github={member.github}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
 
