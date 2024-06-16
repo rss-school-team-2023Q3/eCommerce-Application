ae31d4d1f2ed6ab04d6fa9bde646e42874d4d4b2
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-06-17 01:03:22 +0300

    feat: add promo code info to main

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,11 +1,13 @@
 import { NavLink, Navigate } from 'react-router-dom';
 import './Main.modules.css';
+import PromoCodeComponent from 'shared/components/PromoCodeComponent/PromoCodeComponent';
 
 export default function Main() {
   return (
     <>
       <Navigate to="/main" />
       <h2>Main page</h2>
+      <PromoCodeComponent />
       <div className="main-links">
         <NavLink className="login-link" to="/catalog">
           Catalog
---
+++ src/shared/components/PromoCodeComponent/PromoCodeComponent.tsx
@@ -0,0 +1,65 @@
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import { useState } from 'react';
+
+function PromoCodeComponent() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyCodeToClipboard = () => {
+    navigator.clipboard.writeText('SUMMER').then(() => {
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 1500);
+    });
+  };
+
+  return (
+    <Card
+      style={{
+        maxWidth: 400,
+        margin: 'auto',
+        textAlign: 'center',
+        boxShadow: '12px 12px 20px -12px rgba(58, 91, 182, 0.815)',
+        background:
+          'radial-gradient(circle, rgba(197,224,255,1) 0%, rgba(221,212,255,1) 100%)',
+      }}
+    >
+      <CardContent>
+        <Typography
+          color="#44367b"
+          fontWeight={500}
+          textAlign="left"
+          fontFamily="Montserrat, sans-serif"
+        >
+          Use Promo Code
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          fontFamily="Montserrat, sans-serif"
+          m="10px 0"
+          fontWeight={800}
+          sx={{ background: '#ffffff', borderRadius: '5px' }}
+          color="#1569bd"
+        >
+          SUMMER
+        </Typography>
+        <Typography
+          color="textSecondary"
+          fontFamily="Montserrat, sans-serif"
+          marginBottom={2}
+          fontWeight={500}
+        >
+          Only this summer get a 25% discount on all products!
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={copyCodeToClipboard}
+        >
+          {isCopied ? 'Copied!' : 'Copy Code'}
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default PromoCodeComponent;
