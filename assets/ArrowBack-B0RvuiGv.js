ca6e5b1521515486112f4762eadc241d52b852dc
AntonOgonovskiy <90901486+AntonOgonovskiy@users.noreply.github.com>
2024-06-20 14:06:09 +0000

    Deploying to gh-pages from @ rss-school-team-2023Q3/eCommerce-Application@34bcbbe98078a2d35fc1d95757eb52b35242a505 🚀

---
+++ assets/ArrowBack-B0RvuiGv.js
@@ -0,0 +1,1 @@
+import{b as r,i as a,j as t}from"./index-CTuUUjIM.js";var e={},o=a;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,i=o(r()),l=t;u=e.default=(0,i.default)((0,l.jsx)("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}),"ArrowBack");export{u as d};
